0490655813470d21ece21cf5f93ff38a2405134b
nicobojacarojas <nicobojacarojas@hotmail.com>
2018-04-15 22:19:14 -0500

    segunda version

---
+++ punto_3/fourier.cpp
@@ -0,0 +1,28 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <png.h>
+int main()
+{
+	float num1 = {2., 3.4, 4.8, 6.9, 7.8, 8.10};
+	return 0;
+}
+//funcion cualquiera que toma un numero y devuelve algo corrspondiente a este
+float func(float num1)
+{
+	for (int i = 0; i < GetSize(num1); i++)
+	{
+		float num2[i] = num1[i]*3;
+	}
+	return num2;
+}
+//funcion que realiza la interpolacion de lagrange
+float interla(float num1, float num2)
+{
+	int n = GetSize(num1)
+	for (int i = 0; i < n; i++)
+	{
+		float 1 = 
+	}
+
+}
+
